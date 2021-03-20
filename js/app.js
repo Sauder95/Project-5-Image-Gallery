23854b61c59d89fd45642a1d0f7ce72825dbced8
Sauder95 <63077317+Sauder95@users.noreply.github.com>
2021-03-20 14:15:14 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,28 +2,28 @@ baguetteBox.run('#gallery');
 
 //VARIABLES-------------------
 
-// const input = document.getElementById('search');
-// const thumbnails = document.getElementsByTagName("img");
+const input = document.getElementById('search');
+const thumbnails = document.getElementsByTagName("img");
 
 
 
-// //Event Listners
-// input.addEventListener("keyup", imgFilter);
-// input.addEventListener("keyup", inputLow);
+//Event Listners
+input.addEventListener("keyup", imgFilter);
+input.addEventListener("keyup", inputLow);
 
 
-// function inputLow() {
-//     input.value = input.value.toLowerCase();
-// }
+function inputLow() {
+    input.value = input.value.toLowerCase();
+}
 
-// function imgFilter() {
-//     for (i = 0; i < 12; i++) {
-//         let imgCaption = document.getElementsByTagName("a")[i].getAttribute("data-caption");
-//         imgCaption = imgCaption.toLowerCase();
-//         if (imgCaption.includes(input.value)) {
-//             thumbnails[i].parentNode.parentNode.style.display = "inline";
-//         } else {
-//             thumbnails[i].parentNode.parentNode.style.display = "none";
-//         }
-//     }
-// }
+function imgFilter() {
+    for (i = 0; i < 12; i++) {
+        let imgCaption = document.getElementsByTagName("a")[i].getAttribute("data-caption");
+        imgCaption = imgCaption.toLowerCase();
+        if (imgCaption.includes(input.value)) {
+            thumbnails[i].parentNode.parentNode.style.display = "inline";
+        } else {
+            thumbnails[i].parentNode.parentNode.style.display = "none";
+        }
+    }
+}
